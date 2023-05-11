287b2a42534b6e82baed9d3a5ede1f840fb13d43
Adrish Mitra <adrishmitra710@gmail.com>
2023-05-12 00:45:34 +0530

    slider function implemented

--- 07-Slider/src/App.js
+++ 07-Slider/src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import Carousel from './Carousel';
 function App() {
-  return <h2>slider project setup</h2>;
+  return(
+    <main className='section'>
+      <div className="title">
+        <h2><span>/</span> Reviews</h2>
+      </div>
+        <Carousel/>
+    </main>
+  )
 }
 
 export default App;
---
+++ 07-Slider/src/Carousel.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaQuoteRight } from 'react-icons/fa';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+const Carousel = () => {
+    const [currIndex, setCurrIndex] = useState(0)
+    const next = ()=>{
+        setCurrIndex((currIndex+1) % people.length)
+    }
+    const prev = ()=>{
+        setCurrIndex((currIndex - 1 + people.length) % people.length)
+    }
+
+  return (
+    <div className='section-center'>
+        {people.map((person, index)=>{
+            const {id, image, name, title, quote} = person
+            return(
+                <article key ={id}
+                style={{
+                        transform: `translateX(${100 * (index - currIndex)}%)`, 
+                        opacity: `${index === currIndex? 1: 0}`,
+                        visibility: `${index === currIndex? 'visible' : 'hidden'}`
+                    }
+                }>
+                <img className='person-img' src={image} alt={name} />
+                    <h4>{name}</h4>
+                    <p className='title'>{title}</p>
+                    <p className='text'>{quote}</p>
+                    <FaQuoteRight className='icon'/>
+                </article>
+            )
+        })}
+        <button onClick={prev} className='prev'>
+            <FiChevronLeft/>
+        </button>
+        <button onClick={next} className='next'>
+            <FiChevronRight/>
+        </button>
+    </div>
+  );
+}
+
+export default Carousel;
